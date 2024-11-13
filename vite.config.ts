1ef5c5b8616352d492d801dac68b1905f440516c
Rina Lisa <anpast2014@yandex.ru>
2024-11-13 23:42:10 +0300

    add ignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import stylelint from 'vite-plugin-stylelint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), stylelint({})],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
