d2ea3c756db34f9e9e87b36e7afb877ca674fb05
Andrey Strelnikov <xoz9ih@outlook.com>
2024-11-14 14:24:23 +0300

    wishes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from '/logo.svg'
 import './App.css'
 
 function App() {
@@ -9,25 +8,18 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="#" target="_blank">
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://github.com/RinaLis/rpg_notes_frontend" target="_blank">
+          <img src={logo} className="logo react" alt="NRI logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>НРИ</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
