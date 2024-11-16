ce11ce21e9232f3a35b25abd71409e954e8042eb
mashkay <mashkay@gmail.com>
2024-11-16 11:35:31 +0100

    aliaas

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,22 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': '/src',
+			'@components': '/src/components',
+			'@ui': '/src/components/ui',
+			'@pages': '/src/pages',
+			'@ui-pages': 'src/components/ui/pages',
+			'@assets': '/src/assets',
+			'@hooks': '/src/hooks',
+			'@utils': '/src/utils',
+			'@api': 'src/utils/notes-api.ts',
+			'@services': '/src/services',
+			'@slices': 'src/services/slices',
+			'@store': 'src/services/store.ts',
+		},
+	},
 
 	test: {
 		globals: true,
