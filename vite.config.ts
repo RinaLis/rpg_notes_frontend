ae9669e281b1091191e235c00e0896a421224798
mashkay <mashkay@gmail.com>
2024-11-16 08:00:34 +0100

    test husky

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/__tests__/setup.ts'],
-  },
+	plugins: [react()],
+
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: ['src/__tests__/setup.ts'],
+	},
 });
