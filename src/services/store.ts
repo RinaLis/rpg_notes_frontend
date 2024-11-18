0dd4b26b1881ca2d30fb677fd9a896732690142b
mashkay <mashkay@gmail.com>
2024-11-18 06:16:40 +0100

    настроила store и заготовку для слайса auth

---
+++ src/services/store.ts
@@ -0,0 +1,23 @@
+import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import {
+	TypedUseSelectorHook,
+	useDispatch as dispatchHook,
+	useSelector as selectorHook,
+} from 'react-redux';
+import authSlice from './slices/auth/auth.slice';
+
+export const rootReducer = combineSlices(authSlice);
+
+const store = configureStore({
+	reducer: rootReducer,
+	devTools: process.env.NODE_ENV !== 'production',
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = () => dispatchHook();
+export const useAppSelector: TypedUseSelectorHook<RootState> = selectorHook;
+
+export default store;
