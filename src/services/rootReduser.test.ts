4ab57aa440d6b63140c6933dff82b6673535f231
mashkay <mashkay@gmail.com>
2024-11-18 06:34:43 +0100

    добавила тесты для auth slice и store

---
+++ src/services/rootReduser.test.ts
@@ -0,0 +1,16 @@
+import { rootReducer } from './store';
+import { authSliceInitialState } from './slices/auth/authSlice.test';
+
+const initialState = {
+	auth: authSliceInitialState,
+};
+
+describe('rootReducer', () => {
+	it('Initialization: should return the initial state', () => {
+		expect(rootReducer(undefined, { type: 'unknown' })).toEqual(initialState);
+	});
+
+	it('unnamed action: should return the initial state', () => {
+		expect(rootReducer(initialState, { type: 'unknown' })).toEqual(initialState);
+	});
+});
