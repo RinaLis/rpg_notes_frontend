0a6eac77d1d9165cd7ddf109a3165764340ecd89
Rina Lisa <anpast2014@yandex.ru>
2024-11-21 19:34:53 +0300

    remove slices with bad types

--- src/services/slices/index.ts
+++
@@ -1,6 +0,0 @@
-import { combineSlices } from '@reduxjs/toolkit';
-import authSlice from './auth/auth.slice';
-
-export * as auth from './auth';
-
-export const rootReducer = combineSlices(authSlice);
