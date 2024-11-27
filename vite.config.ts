ac62a482f25ec036d6f03308d42e9624b9e091a1
mashkay <mashkay@gmail.com>
2024-11-27 18:03:49 +0100

    поставила либу для свг написала пример использования

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
+	plugins: [react(), tsconfigPaths(), svgr()],
 	resolve: {
 		alias: {
 			'@': '/src',
