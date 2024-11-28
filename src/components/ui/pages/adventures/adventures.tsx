3342a85820fc6ce1c26ca805799020644abfd5c1
Andrey Strelnikov <xoz9ih@outlook.com>
2024-11-28 22:46:43 +0300

    first cod

--- src/components/ui/pages/adventures/adventures.tsx
+++ src/components/ui/pages/adventures/adventures.tsx
@@ -1,5 +1,24 @@
+import { Button } from '@components';
 import React from 'react';
+import { ReactComponent as Frame } from '@assets/eyeinput.svg';
+import clsx from 'clsx';
+import styles from './adventures.module.scss';
 
 export const AdventuresUI: React.FC = () => {
-	return <div>Adventures</div>;
+	return (
+		<div className={styles.adventures}>
+			<Button
+				type="button"
+				className={clsx(styles.button, styles.button__addPlayer)}
+				onClick={() => {
+					console.log('dell player');
+				}}
+			>
+				<div className={styles.button__addPlayerIconContainer}>
+					<Frame className={styles.button__addPlayerIcon} />
+				</div>
+				<span className={styles.button__addPlayerText}>добавить игрока</span>
+			</Button>
+		</div>
+	);
 };
