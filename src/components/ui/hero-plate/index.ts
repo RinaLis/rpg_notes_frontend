3a4126799fa2798443e99c0676860acbbcbfc9fe
mashkay <mashkay@gmail.com>
2024-12-05 13:34:00 +0100

    заменила кнопку на Link

--- src/components/ui/hero-plate/index.ts
+++ src/components/ui/hero-plate/index.ts
@@ -1,1 +1,2 @@
 export { HeroPlate } from './hero-plate';
+export type { HeroPlateProps } from './type';
--- src/components/ui/hero-plate/type.ts
+++ src/components/ui/hero-plate/type.ts
@@ -2,7 +2,7 @@ export type HeroPlateProps = {
 	heroName?: string;
 	playerName: string;
 	image?: string | null;
-	onClick?: () => void;
 	master?: boolean;
 	dead?: boolean;
+	id: string;
 };
